ea836a3cb2f55e5d396b15e1d220c94572eebce4
Koorosh <kkomeilizadeh@gmail.com>
2025-04-10 12:51:25 +0200

    update(A1): Lani's update on R code

---
+++ Assignment1/RM_assignment.R
@@ -0,0 +1,14 @@
+library(truncnorm)
+n <- 5280
+n_tests <- 10000
+p_out <- numeric(n_tests)
+for (i_test in 1:n_tests){
+  prev_relash <- round(rtruncnorm(n=n, mean=3, sd=4, a=0, b=12))
+  match_rate <- rtruncnorm(n=n, mean=10, sd=15, a=0.5, b=65)
+       
+  model = lm(match_rate ~ prev_relash)
+  model_summary <- summary(model)
+  p_out[i_test] <- model_summary$coefficients[2,4]
+}
+
+hist(p_out)
