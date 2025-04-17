0291f3300abb8be51bae02480f1ad056bd3ef3da
Koorosh <kkomeilizadeh@gmail.com>
2025-04-17 19:49:27 +0200

    update(A1): p-values rounded to below 0.05

--- Assignment1/RM_assignment.R
+++ Assignment1/RM_assignment.R
@@ -1,14 +1,38 @@
-library(truncnorm)
-n <- 5280
-n_tests <- 10000
-p_out <- numeric(n_tests)
+library(truncnorm) # Library to make truncated normal distribution
+
+n <- 5280 # number of sample size per simulation
+n_tests <- 10000 # number of simulations
+p_out <- numeric(n_tests) # vector to store p-values
+
+
 for (i_test in 1:n_tests){
-  prev_relash <- round(rtruncnorm(n=n, mean=3, sd=4, a=0, b=12))
-  match_rate <- rtruncnorm(n=n, mean=10, sd=15, a=0.5, b=65)
+
+  # independent variable: simulate previous relationships
+  prev_relash <- round(
+    rtruncnorm(
+      n = n,      # sample size
+      mean = 3,   # average
+      sd = 4,     # standard deviation
+      a = 0,      # lower bound
+      b = 12      # upper bound
+    )
+  )
+
+  match_rate <- rtruncnorm(n=n, mean=10, sd=15, a=0.5, b=65) # dependent variable
        
-  model = lm(match_rate ~ prev_relash)
+  model = lm(match_rate ~ prev_relash) # linear regression model (dependent (y) ~ independent (x))
   model_summary <- summary(model)
-  p_out[i_test] <- model_summary$coefficients[2,4]
+  
+  # Extract p-value for the relationship (predictor: prev_relash)
+  p_value <- model_summary$coefficients[2,4]
+  
+  # QRP: Rounding p-values down
+  if (p_value > 0.05) {
+    p_value <- 0.05
+  }
+  
+  # Store the adjusted p-value
+  p_out[i_test] <- p_value
 }
 
-hist(p_out)
+hist(p_out) # Histogram of p_out with QRP -> Rounding p-values
